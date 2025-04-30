e176505037d92542127f1447516f23339f4eab72
resousadev <resousadev@gmail.com>
2025-04-29 22:32:33 -0300

    Add SQL Server resource and related variables to Terraform configuration

--- main.tf
+++ main.tf
@@ -18,3 +18,16 @@ resource "azurerm_resource_group" "rg" {
   name     = "lab_dio_azure"
   location = var.location
 }
+
+resource "azurerm_mssql_server" "sql_server" {
+  name                         = var.sql_server_name
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = var.sql_server_version
+  administrator_login          = var.sql_admin_username
+  administrator_login_password = var.sql_admin_password
+
+  public_network_access_enabled = true
+  minimum_tls_version = "1.2"
+}
+
--- variables.tf
+++ variables.tf
@@ -3,3 +3,33 @@ variable "location" {
   type        = string
   default     = "West US 2"
 }
+
+variable "sql_admin_username" {
+  description = "The administrator username for the SQL server."
+  type        = string
+  default     = "sqladmin"
+}
+
+variable "sql_admin_password" {
+  description = "The administrator password for the SQL server."
+  type        = string
+  default     = "P@ssw0rd1234!"
+}
+
+variable "sql_server_name" {
+  description = "The name of the SQL server."
+  type        = string
+  default     = "lab-sql-server-dio-az"
+}
+
+variable "sql_database_name" {
+  description = "The name of the SQL database."
+  type        = string
+  default     = "lab-sql-database"
+}
+
+variable "sql_server_version" {
+  description = "The version of the SQL server."
+  type        = string
+  default     = "12.0"
+}
